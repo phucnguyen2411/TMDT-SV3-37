d7e4f8aea19427875877b33aba15386195c91813
Phuc .Ng <phucnguyen.2411@gmail.com>
2016-04-26 22:39:15 +0700

    [ADD]-Create project

---
+++ TMDT-SV3-37/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TMDT-SV3-37
+//
+//  Created by Phuc .Ng on 4/26/16.
+//  Copyright © 2016 com.phucng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
